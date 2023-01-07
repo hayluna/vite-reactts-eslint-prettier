d91b571baed897eca55adf4ddba2edd59806454b
명혜은 <hayluna2@gmail.com>
2023-01-07 19:22:50 +0900

    chore: package update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root')
+const root = createRoot(container!) // createRoot(container!) if you use TypeScript
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 )
